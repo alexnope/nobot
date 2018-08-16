5317d6cfb84e34f45e4e8ce112cc91fb70b0a13b
no <32372863+alexnope@users.noreply.github.com>
2018-08-15 20:05:54 -0400

    Add files via upload
    -Added some code for MySQL (has issues with server connection)
    -Redid the myrng function based off the middle squared psuedo random number generator

--- rngbot.js
+++ rngbot.js
@@ -2,37 +2,47 @@ const Discord = require("discord.js");
 const CONFIG = require("./config.json");
 const PREFIX = "-";
 const client = new Discord.Client();
-var rngbool = true;
+const mysql = require("mysql");
 //Ready Message
-client.on("ready", function(){console.log("Ready");});  //if success bot will say readys
 
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    port: "3306",
+    password: "",
+    database: "rngdb",
+});
+con.connect(err => {
+    if(err) throw err;
+    console.log("Connected to database");
+});
+
+
+client.on("ready", async ()=> {console.log("RNGBot is Online");});  //if success bot will say readys
+                                                                    //() => == function(args)
 //Client is recieving messages
-client.on("message", function(message) {
+
+client.on("message", async message => {                             //shorthand for function looks better
     if(message.author.equals(client.user)) return;
 
     if(!message.content.startsWith(PREFIX)) return;
 
 //my functions
-function pickrandom(listofpeople){      
-    listofpeople = listofpeople.split(" ");     //make a way to do this with tags instead of just names
+function pickrandom(listofpeople){
+    listofpeople = listofpeople.split(" ");                         //make a way to do this with tags instead of just names
     return listofpeople[myrng(0,listofpeople.length)];
 }
-function myrng(range1,range2){   //creating own random number generator
-    let TIME = process.hrtime();  //grab cpu time
-    let number;
-    let randomseed = TIME[0];
-    if(rngbool == true){
-        randomseed = (TIME[0]*123456);   //take the nano sec * the normal sec
-        number = (randomseed % range2) + range1;
-        rngbool = !rngbool;
+//
+function myrng(range1,range2){
+    //use middle square method
+    let TIME = process.hrtime();                                    //grab cpu time
+    let seed = TIME[1] * TIME[1];
+    while(seed.length > 8){
+        seed.pop();
     }
-    else{
-        randomseed = (randomseed * 9999);
-        number = (randomseed % range2) + range1;
-        rngbool = !rngbool;
-    }
-    return number;   //test print
-    
+    seed = parseInt(seed.toString().substring(2,6))
+    seed = (seed%range2) + range1;
+    return seed;
 }
 
 //variables used throughout
@@ -40,21 +50,22 @@ var servers = {};
 var fortunes = ["yes", "no", "maybe", "probably not", "probably", " not sure man", "ask again" ];
 var coinside = ["Heads", "Tails"]
 var cornside = ["Cob","Kennel"]
-var normalcommands = ["8ball", "coinflip", "lottery"]       //add command here to add the help function
+var dice = [":one:",":two:",":three:",":four:",":five:",":six:"]
+var normalcommands = ["8ball", "coinflip", "lottery", "cornflip", "rolldice"]       //add command here to add the help function
 
-var args = message.content.substring(PREFIX.length).split(" "); //remove prefix for case statement
- 
+var args = message.content.substring(PREFIX.length).split(" ");      //remove prefix for case statement
+
 //start case statement, list of commands
-switch(args[0].toLowerCase()) {          
+switch(args[0].toLowerCase()) {
     case "help":
-            var embed = new Discord.RichEmbed()
+            let embed = new Discord.RichEmbed()
                 .setAuthor("NoBot", client.user.displayAvatarURL)
                 .setDescription("Bot Alex No created to play around with node js")
                 .addField("Command list",normalcommands)
                 .setColor(0xF4F766)
             message.channel.send(embed);
             break;
-            
+
         case "info":
             message.channel.send("Bot specifically made for random commands");
             break;
@@ -69,14 +80,20 @@ switch(args[0].toLowerCase()) {
         case "coinflip":
             message.channel.send(coinside[myrng(0,2)]);
             break;
-        
+
         case "cornflip":
             message.channel.send(cornside[myrng(0,2)]);
             break;
+
         case "lottery":
             let listofpeople = message.content.substring(PREFIX.length+8);
             message.channel.send(pickrandom(listofpeople));
             break;
+
+        case "rolldice":
+            message.channel.send(dice[myrng(0,5)]);
+            break;
+
         default:
             message.channel.send("Not a valid command");
     }
